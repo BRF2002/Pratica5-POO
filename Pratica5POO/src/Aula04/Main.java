807b1345328a9fddbce6d9141705998ff687bd5d
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-05-10 22:15:02 -0300

    Add files via upload

---
+++ Pratica5POO/src/Aula04/Main.java
@@ -0,0 +1,19 @@
+package Aula04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Pessoa func1 = new Pessoa ("Lewandowski",185,75);
+		Pessoa func2 = new Pessoa ("Haaland", 194, 88);
+		Pessoa func3 = new Pessoa ("Suarez", 182, 83);
+		
+		Pessoa [] pessoas = new Pessoa [10];
+		for (int i=0; i<10; i++)
+		{
+			pessoas[i] = new Pessoa ("Lewandowski"+i,50,1.71);
+		}
+		}
+	}
+
+
